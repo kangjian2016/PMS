d277cd3a282725c14839e3d415b002b9ba748a1c
Jian Kang <jiankang@Jians-MacBook-Pro.local>
2021-03-08 22:06:48 -0500

    remove Makevars

--- src/PMS.cpp
+++ src/PMS.cpp
@@ -25,13 +25,12 @@ Rcpp::List fast_PMS_cpp(arma::mat &x, arma::vec& y, double theta=1e-3){
 Rcpp::List fast_data_gen(int n, int p, double R2, arma::vec& beta_nonzero){
   arma::mat x = arma::randn<arma::mat>(n,p);
   arma::vec z = arma::randn<arma::vec>(n);
-  for(int i=0;i<x.n_cols;i++){
+  for(arma::uword i=0;i<x.n_cols;i++){
   	x.col(i) += z;
   }
   arma::vec y = x.cols(0,beta_nonzero.n_elem-1L)*beta_nonzero;
   double var_y = arma::var(y);
   y += arma::randn(n)*sqrt(var_y*(1.0 - R2)/R2);
-
   return Rcpp::List::create(Named("x") = x,
                             Named("y") = y);
 }
